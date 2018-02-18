ea2f0cd795733143280e50db32b8bec14851e848
Patrik Keller <patrik@keller-re.de>
2018-02-18 16:46:30 +0100

    Prepare status handling

---
+++ lib/status.mli
@@ -0,0 +1,7 @@
+exception Notmuch_Error of string
+
+(* Check error code and raise Notmuch_error if neccessary *)
+val throw : C.status -> unit
+
+(* Check error code and return error message option *)
+val check : C.status -> string option
