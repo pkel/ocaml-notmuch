21696e4c9fac5f211af9f68757d99ddc666949b7
Patrik Keller <patrik@keller-re.de>
2018-02-18 17:26:26 +0100

    Use Status module in message module

--- lib/message.ml
+++ lib/message.ml
@@ -1,8 +1,6 @@
 type t = C.message
 
-let throw err = function
-  | 0 -> ()
-  | _ -> failwith err
+let throw = Status.throw
 
 (* val get_tags : t -> string list *)
 let get_tags msg =
@@ -12,18 +10,18 @@ let get_tags msg =
 
 (* val add_tags : t -> string list -> unit *)
 let add_tags msg lst =
-  let f tag = C.message_add_tag msg tag |> throw "message_add_tags" in
+  let f tag = C.message_add_tag msg tag |> throw in
   List.iter f lst
 
 (* val remove_tags : t -> string list -> unit *)
 let remove_tags msg lst =
-  let f tag = C.message_remove_tag msg tag |> throw "message_remove_tag" in
+  let f tag = C.message_remove_tag msg tag |> throw in
   List.iter f lst
 
 (* val set_tags : t -> string list -> unit *)
 let set_tags msg lst =
   (* safe setting of tags as discussed in notmuch.h *)
-  C.message_freeze msg |> throw "message_freeze" ;
-  C.message_remove_all_tags |> throw "message_remove_all_tags";
+  C.message_freeze msg |> throw ;
+  C.message_remove_all_tags msg |> throw ;
   add_tags msg lst ;
-  C.message_thaw |> throw "message_thaw"
+  C.message_thaw msg |> throw
