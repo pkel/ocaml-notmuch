b5999c00022f878601b667e9a38b61f58f78bfab
Patrik Keller <patrik@keller-re.de>
2018-03-09 16:17:35 +0100

    Introduce tag based rules for moving

--- example/move/move.ml
+++ example/move/move.ml
@@ -8,9 +8,64 @@ let get_db () =
   | None -> failwith "Could not open database"
   | Some db -> db
 
-let folders msg =
-  (* TODO: This is boring *)
-  [ "/home/patrik/mail/hetzner/seen/" ]
+type tag = string
+type folder = string
+type selector = tag list
+
+type rule =
+  | Add of folder
+  | Fst of (selector * rule) list
+  | All of (selector * rule) list
+
+let default = []
+let tag s = [s]
+let tags l = l
+
+let folder_per_tag ?(all=false) ?(prefix="") tags =
+  let f s = tag s , Add (prefix ^ s) in
+  let rules = List.map ~f tags in
+  if all then All rules else Fst rules
+
+let folder_rule : rule =
+  All [
+    tag "todo" , Add "hetzner/todo" ;
+    default    , Fst [
+      tag "spam"  , Add "hetzner/spambucket" ;
+      tag "trash" , Add "hetzner/Trash" ;
+      tag "inbox" , Fst [
+        tag "uibk"    , Add "uibk/INBOX" ;
+        tag "student" , Add "uibk-student/INBOX" ;
+        default       , Add "hetzner/INBOX" ] ;
+      tag "sent"  , All [
+        tag "uibk"    , Add "uibk/Sent" ;
+        tag "student" , Add "uibk-student/Sent" ;
+        tag "private" , Add "hetzner/Sent" ] ;
+      default     , folder_per_tag  ~all:true ~prefix:"hetzner/" [
+        "order"; "invoice"; "masterthesis"; "booking" ] ]
+  ]
+
+let default_folder = "hetzner/default"
+
+module Set = Set.Make(String)
+
+let folders rule msg =
+  let open Set in
+  let msg_tags = Message.get_tags msg |> of_list in
+  let rec h acc rule =
+    let f (selector, rule) =
+      (* apply rule if selector triggers *)
+      if is_subset (of_list selector) ~of_:msg_tags
+      then Some (h acc rule)
+      else None
+    in
+    match rule with
+    | Fst lst -> List.find_map ~f lst |> Option.value ~default:acc
+    | All lst -> List.filter_map ~f lst |> List.fold ~init:acc ~f:union
+    | Add folder -> add acc folder
+  in
+  match h empty rule |> to_list with
+  | [] -> [ default_folder ]
+  | lst -> lst
 
 type mds =
   | Cur
@@ -28,10 +83,13 @@ type location =
   ; file: string
   }
 
+let relative = FilePath.make_relative db_loc
+let absolute = FilePath.make_absolute db_loc
+
 let location_of_filepath fp =
   let file = FilePath.basename fp in
   let dir = FilePath.dirname fp in
-  let folder = FilePath.dirname dir in
+  let folder = FilePath.dirname dir |> relative in
   let state =
     match FilePath.basename dir with
     | "cur" -> Cur
@@ -44,13 +102,17 @@ let location_of_filepath fp =
 let filepath_of_location { file; state; folder } =
   let d = mds_to_string state in
   FilePath.make_filename [ folder; d; file ]
+    |> absolute
 
 let describe msg add del =
   let open Printf in
-  let rel = FilePath.make_relative db_loc in
-  let minus loc = printf " - %s\n" (rel loc.folder) in
-  let plus s = printf " + %s\n" (rel s)in
-  printf "id:%s\n" (Message.get_id msg) ;
+  let minus loc = printf " - %s\n" loc.folder in
+  let plus s = printf " + %s\n" s in
+  if not ( add = [] && del = [] )
+  then
+    Message.get_tags msg
+    |> String.concat ~sep:", "
+    |> printf "tag: %s\n" ;
   List.iter ~f:minus del ;
   List.iter ~f:plus add
 
@@ -73,7 +135,7 @@ let mover move verbose query =
     in
     (* calculate diff *)
     let was = List.map ~f:(fun x -> x.folder) locations in
-    let will = folders msg in
+    let will = folders folder_rule msg in
     let equal a b = if Pervasives.compare a b = 0 then true else false in
     let del =
       List.filter
