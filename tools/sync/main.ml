3dbc5daf3b793238974197f39d69ad6f11896463
Patrik Keller <patrik@keller-re.de>
2018-03-19 23:14:34 +0100

    Syncer can write tags to local git store

--- tools/sync/main.ml
+++ tools/sync/main.ml
@@ -1,22 +1,29 @@
-open Core
 open Notmuch
 
-let syncer ~srch_str db store =
+let syncer db store srch_lst =
+  let user = Unix.getlogin () in
+  let host = Unix.gethostname () in
   let module Store = TagStore.Make(struct
+    open TagStore
+    let author =
+      { name = "notmuch-sync"
+      ; mail = user ^ "@" ^ host
+      }
     let location = store
   end) in
-  let f msg =
+  let f acc msg =
     let id = Message.get_id msg in
     let tags = Message.get_tags msg in
-    Store.update id tags
+    (id, tags) :: acc
   in
   let open Query in
-  from_string srch_str |> Messages.iter db ~f
+  String.concat " " srch_lst |> from_string |> Messages.fold ~init:[] db ~f
+  |> Store.update
 
 let with_db_and_store f =
   let open Printf in
   let open Ext.Result in
-  fun () -> Config.load ()
+  fun x -> Config.load ()
   |> and_then_pair ~fpair:(
     ( fun cfg ->
       Config.get ~section:"database" ~key:"path" cfg
@@ -25,19 +32,16 @@ let with_db_and_store f =
       Config.get ~section:"ocaml" ~key:"sync_store" cfg )
   |> function
     | Error e -> eprintf "%s\n" e
-    | Ok (db, store) -> f db store
+    | Ok (db, store) -> f db store x
 
-let () =
-  let open Command.Let_syntax in
-  Command.basic
-    ~summary:"Sync tags to git repository"
-    [%map_open
-      let srch_lst = anon (sequence ("search-term" %: string))
-      in
-      let srch_str =
-        match srch_lst with
-        | [] -> "*"
-        | l  -> String.concat ~sep:" " l
-      in
-      syncer  ~srch_str |> with_db_and_store
-    ] |> Command.run
+open Cmdliner
+
+let srch_lst =
+  let doc = "Overwrite the deafult search term" in
+  let env = Arg.env_var "NOTMUCH_SEARCH_TERM" ~doc in
+  let doc = "Notmuch search term to filter operation on" in
+  Arg.(value & pos_all string ["path:**"] & info [] ~env ~docv:"SEARCH_TERM" ~doc)
+
+let main_t = Term.(const (with_db_and_store syncer) $ srch_lst)
+
+let () = Term.exit @@ Term.eval (main_t, Term.info "notmuch-sync")
--- tools/sync/tagStore.ml
+++ tools/sync/tagStore.ml
@@ -1,9 +1,11 @@
 module type M = sig
-  val update : string -> string list -> unit
+  val update : (string * string list) list -> unit
 end
 
+type author = { name : string; mail : string}
 module type Cfg = sig
   val location : string
+  val author : author
 end
 
 module Make(Cfg:Cfg) : M = struct
@@ -13,12 +15,29 @@ module Make(Cfg:Cfg) : M = struct
 	module Store = Irmin_unix.Git.FS.KV (Irmin.Contents.String)
 
 	let config = Irmin_git.config ~bare:true Cfg.location
-	let info fmt = Irmin_unix.info ~author:"me <me@moi.com>" fmt
+	let info fmt =
+    let author = Printf.sprintf "%s <%s>" Cfg.author.name Cfg.author.mail in
+    info ~author fmt
+(*  *)
+  (* let set_single t (id, tags) = *)
+    (* let info = info "Updating tags" in *)
+    (* let value = String.concat "|" tags in *)
+    (* Store.set t ~info ["messages";id;"tags"] value *)
+(*  *)
+  let set_lst lst t_opt =
+    let upd acc (id,tags) =
+      let value = String.concat "|" tags in
+      Store.Tree.add acc ["tags"; id] value
+    in
+    let updt t = Lwt_list.fold_left_s upd t lst in
+    match t_opt with
+    | None -> Lwt.return None
+    | Some t -> updt t >>= fun x -> Lwt.return (Some x)
 
-  let update id tags =
+  let update lst =
 		begin
 			Store.Repo.v config >>= Store.master >>= fun t ->
-			Store.set t ~info:(info "Updating foo/bar") ["foo"; "bar"] "hi!"
+        Store.with_tree t [] ~info:(info "Update tags") (set_lst lst)
 		end
     |> Lwt_main.run
 end
