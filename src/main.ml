3602f086edb90a4509071a1cf8c6f94add099c2e
Patrik Keller <patrik@keller-re.de>
2018-03-28 13:20:03 +0200

    Fix main command

--- src/main.ml
+++ src/main.ml
@@ -3,8 +3,8 @@ open Term
 
 let default_cmd =
   let doc = "Ocaml notmuch tools" in
-  Term.(ret (`Help (`Pager, None))),
-  Term.info "onm" ~version:"v1.0.2" ~doc
+  `Help (`Pager, None) |> const |> ret,
+  info "onm" ~version:"v1.0.2" ~doc
 
 let () = exit @@
   eval_choice default_cmd [Move.main_cmd; Sync.main_cmd]
