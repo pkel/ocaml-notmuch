7a19b8ed1e5f307bbf12704ba477a80b99381705
Patrik Keller <patrik@keller-re.de>
2018-03-29 12:12:51 +0200

    Split sync command into subcommands

--- src/main.ml
+++ src/main.ml
@@ -2,9 +2,10 @@ open Cmdliner
 open Term
 
 let default_cmd =
-  let doc = "Ocaml notmuch tools" in
+  let doc = "ocaml notmuch tools - sync tags and reorganize maildir" in
   `Help (`Pager, None) |> const |> ret,
-  info "onm" ~version:"v1.0.2" ~doc
+  info "onm" ~version:"v0.1.0" ~doc
 
 let () = exit @@
-  eval_choice default_cmd [Move.main_cmd; Sync.main_cmd]
+  eval_choice default_cmd [Move.main_cmd; Sync.write_cmd; Sync.pull_cmd;
+  Sync.push_cmd]
--- src/sync/sync.ml
+++ src/sync/sync.ml
@@ -1,81 +1,103 @@
 open Notmuch
 open Lwt.Infix
 
-let syncer ~db ~store ~remote ~srch_lst =
-  let user = Unix.getlogin () in
-  let host = Unix.gethostname () in
-  let module Config = struct
-    open TagStore
-    let author =
-      { name = "notmuch-sync"
-      ; mail = user ^ "@" ^ host
-      }
-    let location = store
-  end in
-  let module Store = TagStore.Make(Config) in
-  let main =
-    (* Pull remote git, merge changes *)
-    Store.pull remote
-    >>= begin function
-      | Ok () -> Lwt.return ()
-      | Error e -> Lwt_io.printf "Failed to pull: %s\n" e
-    end
-    (* Update local git with tags *)
-    >>= fun () ->
-      let f msg =
-        let id = Message.get_id msg in
-        let tags = Message.get_tags msg in
-        (id, tags)
-      in
-      let open Query in
-      String.concat " " srch_lst
-      |> from_string
-      |> Messages.stream db
-      |> Lwt_stream.map f
-      |> Store.set_mtags_stream
-    (* Push stuff to remote *)
-    >>= fun () ->
-      Store.push remote
-    >>= begin function
-      | Ok () -> Lwt.return ()
-      | Error e -> Lwt_io.printf "Failed to push: %s\n" e
-    end
-  in
-  Lwt_main.run main
+module StoreConfig( C : sig val location : string end ) = struct
+  let author =
+    let open TagStore in
+    let user = Unix.getlogin () in
+    let host = Unix.gethostname () in
+    { name = "onm sync"
+    ; mail = user ^ "@" ^ host
+    }
+  let location = C.location
+end
 
-let with_config f =
-  let open Printf in
-  let open Ext.Result in
-  Config.load ()
-  |> and_then_pair ~fpair:
-    ( ( fun cfg ->
-      Config.get ~section:"database" ~key:"path" cfg
-      |> and_then_opt ~err:"Failed to open database" ~f:Database.open_ )
-    , ( fun cfg ->
-      ["sync_store_local"; "sync_store_remote"]
-      |> List.map (fun key -> Config.get ~section:"ocaml" ~key cfg)
-      |> all )
-    )
-  |> function
-    | Error e -> eprintf "%s\n" e
-    | Ok (db , [store; remote]) -> f ~db ~store ~remote
-    | Ok _ -> assert false
+let fail_opt ?(code=1) ~err = function
+  | Some v -> v
+  | None -> (Printf.eprintf "%s\n" err ; exit code)
+
+let fail ?(code=1) = function
+  | Ok v -> v
+  | Error e -> (Printf.eprintf "%s\n" e ; exit code)
+
+module ConfigFail = struct
+  let load () = Config.load ()   |> fail ~code:121
+  let get ~section ~key cfg =
+    Config.get ~section ~key cfg |> fail ~code:122
+end
+
+let write srch_lst =
+  let module C = ConfigFail in
+  let cfg = C.load () in
+  let db = C.get ~section:"database" ~key:"path" cfg
+    |> Database.open_
+    |> fail_opt ~err:"Failed to open database" ~code:123
+  in
+  let local  = C.get ~section:"ocaml" ~key:"sync_store_local"  cfg in
+  let module Store =
+    TagStore.Make(StoreConfig(struct let location = local end))
+    in
+  let lwt =
+    let f msg =
+      let id = Message.get_id msg in
+      let tags = Message.get_tags msg in
+      (id, tags)
+    in
+    let open Query in
+    String.concat " " srch_lst
+    |> from_string
+    |> Messages.stream db
+    |> Lwt_stream.map f
+    |> Store.set_mtags_stream
+  in
+  Lwt_main.run lwt
+
+let read = write
+
+let pull () =
+  let module C = ConfigFail in
+  let cfg = C.load () in
+  let local  = C.get ~section:"ocaml" ~key:"sync_store_local"  cfg in
+  let remote = C.get ~section:"ocaml" ~key:"sync_store_remote" cfg in
+  let module Store =
+    TagStore.Make(StoreConfig(struct let location = local end))
+    in
+  Store.pull remote |> Lwt_main.run |> fail ~code:5
+
+let push () =
+  let module C = ConfigFail in
+  let cfg = C.load () in
+  let local  = C.get ~section:"ocaml" ~key:"sync_store_local"  cfg in
+  let remote = C.get ~section:"ocaml" ~key:"sync_store_remote" cfg in
+  let module Store =
+    TagStore.Make(StoreConfig(struct let location = local end))
+    in
+  Store.push remote |> Lwt_main.run |> fail ~code:5
 
 open Cmdliner
+open Term
+
+let push_cmd =
+  let doc = "Push local changes to remote" in
+  const push $ const (), info ~doc "push"
+
+let pull_cmd =
+  let doc = "Pull remote changes to local" in
+  const pull $ const (), info ~doc "pull"
 
 let srch_lst =
   let doc = "Overwrite the default search term" in
   let env = Arg.env_var "NOTMUCH_SEARCH_TERM" ~doc in
   let doc = "Notmuch search term to filter operation on" in
-  Arg.(value & pos_all string ["path:**"] & info [] ~env ~docv:"SEARCH_TERM" ~doc)
+  Arg.(value
+    & pos_all string ["path:**"]
+    & info [] ~env ~docv:"SEARCH_TERM" ~doc )
 
-open Term
+let write_cmd =
+  let doc = "Write changes to local store" in
+  const write $ srch_lst, info ~doc "write"
 
-let wrapper srch_lst =
-  syncer ~srch_lst |> with_config
-
-let main_cmd =
-  let doc = "Sync tags via irmin" in
-  const wrapper $ srch_lst,
-  info "sync" ~doc
+let read_cmd =
+  let doc = "Read changes from local store" in
+  const read $ srch_lst, info ~doc "read"
 
